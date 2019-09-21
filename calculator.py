0f3d702f290831d32f8cf6c392efc32380d3fc69
Arghya Basu <arghyabs@gmail.com>
2019-09-21 19:51:42 +0000

    Adding multiplication method

--- calculator.py
+++ calculator.py
@@ -15,3 +15,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
